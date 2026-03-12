b4067884a83794161ff891b2732745d87d8bc4b1
Andrew Pitt <apitt@redhat.com>
2026-03-12 16:28:25 +0000

    Update app

--- src/main/java/ca/pitt/demo/idp/skupperbackend/resource/Welcome.java
+++ src/main/java/ca/pitt/demo/idp/skupperbackend/resource/Welcome.java
@@ -1,16 +1,21 @@
-package ca.pitt.demo.idp.skupperbackend.repository;
+package ca.pitt.demo.idp.skupperbackend.resource;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/server")
 public class Welcome {
+
+    @ConfigProperty(name = "current.server")
+    String server;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String welcome() {
-        return "welcome";
+    public String getServer() {
+        return this.server;
     }
 }
